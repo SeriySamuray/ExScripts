f34ddf0ba1b28f2731fe90e8c36823a45b467d94
Sergei <sergpav92@mail.ru>
2025-11-14 12:23:42 +0300

    add check file or dir

--- Utils/CameraReboot.sh
+++ Utils/CameraReboot.sh
@@ -4,12 +4,19 @@
 # Camera Reboot Script #
 ########################
 if [ -z "$1" ]; then
-    echo "Error, no <Station>. Use: $0 <Station>"
+    echo "Error, no <Station>." 
+	echo "Use: $0 <Station>"
     exit 1
 fi
 Station=$1
+PATH_CONFIG=/home/pi/source/Stations/$Station/.config
 
-# reboot
-TF_CPP_MIN_LOG_LEVEL=3 python -m Utils.CameraControl reboot --config /home/pi/source/Stations/$Station/.config
+if [ -e "$PATH_CONFIG" ]; then
+    # reboot
+	TF_CPP_MIN_LOG_LEVEL=3 python -m Utils.CameraControl reboot --config $PATH_CONFIG
+else
+    echo "No such file or directory."
+	echo "Check <Station>"
+fi
 
 exit 0
