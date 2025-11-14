d4f4be3acb3ff58f48e16855814df4cfb960a00a
Sergei <sergpav92@mail.ru>
2025-11-14 12:47:30 +0300

    add print in Main

--- MainExScripts/MainExScript.sh
+++ MainExScripts/MainExScript.sh
@@ -34,10 +34,10 @@ rmsExternal() {
 
 	printf "\nCurrent captured dir: \n$capt \n==============================================================\n"
 
-	#printf "\n\n1. Reboot camera\n================\n"
+	printf "\n\n1. Reboot camera\n================\n"
 	"$LOCATION/ExScripts/Utils/CameraReboot.sh" $Station
 
-	#printf "\n\n2. Upload CapturedStack to www.starvisor.ru\n===========================================\n"
+	printf "\n\n2. Upload CapturedStack to www.starvisor.ru\n===========================================\n"
 	"$LOCATION/ExScripts/Starvisor/capstack.sh" $capt
 
 	printf "\n\n3. Upload csv-files to cloud storages\n=====================================\n"
